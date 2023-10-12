1922c1b6f985726941013655cd8ae29d7a44ccbe
sudeenshrestha <88490655+sudeenshrestha@users.noreply.github.com>
2023-10-12 11:41:06 +0545

    complete js ex1

---
+++ Javascript/ex1/script.js
@@ -0,0 +1,14 @@
+var obj = {
+    name: "Sudeen Shrestha",
+    address: "Kathmandu",
+    email: "sudeenshrestha@outlook.com",
+    interests: [
+        { interest: "reading" },
+        { interest: "gaming" },
+        { interest: "programming" },
+    ],
+    education: "Civil Engineering"
+
+}
+
+console.log(obj.name)
