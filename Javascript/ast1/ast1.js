29b479d799f16ecbd66e1121134482ea105512ac
sudeenshrestha <88490655+sudeenshrestha@users.noreply.github.com>
2023-10-12 12:56:42 +0545

    complete animate part js ast1

---
+++ Javascript/ast1/ast1.js
@@ -0,0 +1,28 @@
+var star = "*";
+let incrementCount = 1;
+let decrementCount = 6;
+
+let stepCount = 0;
+
+function animate() {
+  if (incrementCount < 6) {
+    var incrementStars = star.repeat(incrementCount);
+    console.log(incrementStars);
+    incrementCount++;
+  }
+
+  else if (incrementCount >= 6) {
+    var decrementStars = star.repeat(decrementCount);
+    console.log(decrementStars);
+    decrementCount--;
+  }
+}
+
+let interval = setInterval(() => {
+  animate();
+  stepCount++;
+
+  if (stepCount == 20) {
+    clearInterval(interval);
+  }
+}, 1000);
