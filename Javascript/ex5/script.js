a1cafa1a3e546f84459f9941f1b0c87a55a9324e
sudeenshrestha <88490655+sudeenshrestha@users.noreply.github.com>
2023-10-13 10:50:51 +0545

    corrected file complete js ex5

--- Javascript/ex5/script.js
+++ Javascript/ex5/script.js
@@ -2,13 +2,16 @@
 // Program to toggle background of a div
 
 var colors = ["red", "blue", "green", "yellow"];
-var divC = document.getElementById("color");
+var divCurrent = document.getElementById("color");
 
-for (let i = 0; i < colors.length; i++) {
-  divC.onclick = function () {
-    divC.style.background = colors[i];
-  };
-  if (colors[0] == colors.slice(-1)[0]) {
-    console.log("ok");
+currentColorCount = 0;
+
+divCurrent.onclick = function () {
+  divCurrent.style.background = colors[currentColorCount];
+
+  currentColorCount++;
+
+  if (currentColorCount === colors.length) {
+    currentColorCount = 0;
   }
-}
+};
