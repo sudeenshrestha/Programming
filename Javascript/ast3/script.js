7005c6fc69657e336dd68096bf0dc19c3af16b66
sudeenshrestha <88490655+sudeenshrestha@users.noreply.github.com>
2023-10-17 14:38:14 +0545

    workflow complete js ast3

--- Javascript/ast3/script.js
+++ Javascript/ast3/script.js
@@ -1,18 +1,6 @@
-// const btnPrevious = document.createElement('button');
-// btnPrevious.textContent = 'Previous';
-
-// const btnNext = document.createElement('button');
-// btnNext.textContent = 'Next';
-
-// const body = document.getElementsByTagName('body')[0];
-// body.appendChild(btnNext);
-// body.appendChild(btnPrevious);
-
-//////////////////////////////////////////////////////
-
 let images = ["./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg"];
+let imageIndex = 0;
 
-// let currentPhotoCount = 1;
 function imgCreate() {
   var img = document.createElement("img");
   img.src = images[0];
@@ -21,50 +9,34 @@ function imgCreate() {
   img.alt = "Picture";
 
   document.body.appendChild(img);
-  let currentPhotoCount = 1;
 
   const next = document.getElementById("next");
 
   next.addEventListener("click", function () {
-    console.log(currentPhotoCount);
-    if (currentPhotoCount === 0) {
-      img.src = images[0];
-      currentPhotoCount++;
-      return img.src;
+    let newImageIndex = imageIndex + 1;
+    if (newImageIndex >= images.length) {
+      newImageIndex = 0;
     }
-    console.log(currentPhotoCount);
-    
-    if (currentPhotoCount>images.length){
-        currentPhotoCount === images.length-1;
-    }
-    img.src = images[currentPhotoCount];
-    console.log(img.src = images[currentPhotoCount]);
-    currentPhotoCount++;
-    console.log("finalnext", currentPhotoCount);
-    console.log(images.length);
 
-    if (currentPhotoCount === images.length) {
-      currentPhotoCount = 0;
-    }
+    const image = images[newImageIndex];
+    imageIndex = newImageIndex;
+    img.src = image;
+    console.log(newImageIndex);
   });
 
   const previous = document.getElementById("previous");
 
   previous.addEventListener("click", function () {
-    
-    console.log(currentPhotoCount);
-    img.src = images[currentPhotoCount-2];
-    console.log(img.src);
-    console.log(currentPhotoCount);
+    let newImageIndex = imageIndex - 1;
+    console.log(newImageIndex);
 
-    currentPhotoCount--;
-    console.log("final:", currentPhotoCount);
-    if(currentPhotoCount <2){
-        currentPhotoCount = images.length+1;
+    if (newImageIndex < 0) {
+      newImageIndex = images.length - 1;
     }
 
-    
-    // previousCount = images.length;
+    let image = images[newImageIndex];
+    imageIndex = newImageIndex;
+    img.src = image;
   });
 }
 imgCreate();
